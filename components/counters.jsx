f52d168694e94ed9897363055540e050fc2bab10
Marques Reimann <Marques_Reimann@yahoo.com>
2019-05-29 23:36:46 -0700

    Add files via upload
    divs added

--- components/counters.jsx
+++ components/counters.jsx
@@ -4,9 +4,10 @@ import Counter from "./counter";
 class Counters extends Component {
   render() {
     console.log("Counters - Rendered");
-    const { onReset, counters, onDelete, onIncrement } = this.props;
+    const { onReset, counters, onDelete, onIncrement, onReduce } = this.props;
     return (
       <div>
+        <br />
         <button onClick={onReset} className="btn btn-primary btn-sm m-2">
           Reset
         </button>
@@ -15,9 +16,12 @@ class Counters extends Component {
             key={counter.id}
             onDelete={onDelete}
             onIncrement={onIncrement}
+            onReduce={onReduce} //lifts the state via props
             counter={counter}
           />
         ))}
+        <br />
+        <br />
       </div>
     );
   }
